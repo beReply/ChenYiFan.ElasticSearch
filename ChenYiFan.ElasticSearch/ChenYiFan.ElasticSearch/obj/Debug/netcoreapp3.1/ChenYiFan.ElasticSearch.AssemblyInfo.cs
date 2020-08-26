77a61b876a4a05f78468eff439e6dbac2b6badf0
beReply <2366337107@qq.com>
2020-08-26 22:06:24 +0800

    添加url

--- ChenYiFan.ElasticSearch/ChenYiFan.ElasticSearch/obj/Debug/netcoreapp3.1/ChenYiFan.ElasticSearch.AssemblyInfo.cs
+++ ChenYiFan.ElasticSearch/ChenYiFan.ElasticSearch/obj/Debug/netcoreapp3.1/ChenYiFan.ElasticSearch.AssemblyInfo.cs
@@ -13,11 +13,11 @@ using System.Reflection;
 
 [assembly: System.Reflection.AssemblyCompanyAttribute("none")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
-[assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.4.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.4")]
+[assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.5.0")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.5")]
 [assembly: System.Reflection.AssemblyProductAttribute("ChenYiFan.ElasticSearch")]
 [assembly: System.Reflection.AssemblyTitleAttribute("ChenYiFan.ElasticSearch")]
-[assembly: System.Reflection.AssemblyVersionAttribute("1.0.4.0")]
+[assembly: System.Reflection.AssemblyVersionAttribute("1.0.5.0")]
 
 // 由 MSBuild WriteCodeFragment 类生成。
 
